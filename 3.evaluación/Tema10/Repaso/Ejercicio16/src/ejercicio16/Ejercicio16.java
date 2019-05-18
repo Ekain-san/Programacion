bb8ef125a1025aadae80ece50f842565f6bd67b8
Ekain <ekain.armendaritz@ikasle.egibide.org>
2019-05-18 13:35:01 +0200

    Repaso 16

---
+++ 3.evaluación/Tema10/Repaso/Ejercicio16/src/ejercicio16/Ejercicio16.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejercicio16;
+
+/**
+ *
+ * @author Asus
+ */
+public class Ejercicio16 {
+
+    /**
+     * @param args the command line arguments
+     */
+    private static int x = (int) (Math.random()*25+1);
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
