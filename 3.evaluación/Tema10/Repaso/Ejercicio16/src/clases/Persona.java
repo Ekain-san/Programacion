e36a1244cc191ae7f28ec35cbeb4195c2413d246
Ekain <ekain.armendaritz@ikasle.egibide.org>
2019-05-18 17:53:26 +0200

    Repaso 16

--- 3.evaluación/Tema10/Repaso/Ejercicio16/src/clases/Persona.java
+++ 3.evaluación/Tema10/Repaso/Ejercicio16/src/clases/Persona.java
@@ -10,5 +10,17 @@ package clases;
  * @author Asus
  */
 public class Persona {
-    
+    private int edad;
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public Persona(int edad) {
+        this.edad = edad;
+    }
 }
--- 3.evaluación/Tema10/Repaso/Ejercicio16/src/ejercicio16/Ejercicio16.java
+++ 3.evaluación/Tema10/Repaso/Ejercicio16/src/ejercicio16/Ejercicio16.java
@@ -5,6 +5,9 @@
  */
 package ejercicio16;
 
+import clases.Persona;
+import java.util.ArrayList;
+
 /**
  *
  * @author Asus
@@ -14,10 +17,47 @@ public class Ejercicio16 {
     /**
      * @param args the command line arguments
      */
-    private static int x = (int) (Math.random()*25+1);
+    private static int x = (int) (Math.random()*50+0);
+    private static int y = (int) (Math.random()*60+5);
+    private static int cantidad;
+    
+    private static double precio;
+    private static double total = 0;
+    
+    private static ArrayList <Persona> cola = new ArrayList <Persona> ();
+    
+    private static Persona persona;
     
     public static void main(String[] args) {
-        // TODO code application logic here
+        rellenar();
+        precio();
+    }
+
+    private static void rellenar() {
+        for(cantidad = 0; cantidad < x; cantidad++){
+            persona.setEdad(y);
+            cola.add(persona);
+        }
+    }
+
+    private static void precio() {
+        for(cantidad = 0; cantidad < x; cantidad++){
+            if(3<cola.get(x).getEdad()&&cola.get(x).getEdad()<11){
+                precio = 1;
+            }
+            
+            if(10<cola.get(x).getEdad()&&cola.get(x).getEdad()<18){
+                precio = 2.5;
+            }
+            
+            if(17<cola.get(x).getEdad()){
+                precio = 5;
+            }
+            
+            cola.remove(x);
+            
+            total = total + precio;
+        }
     }
     
 }
