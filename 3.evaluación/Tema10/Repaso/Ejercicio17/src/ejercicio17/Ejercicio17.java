35c8aafcd6829bd738b5701e00df26bc989506d8
Ekain <ekain.armendaritz@ikasle.egibide.org>
2019-05-21 08:53:38 +0200

    repaso 18

--- 3.evaluación/Tema10/Repaso/Ejercicio17/src/ejercicio17/Ejercicio17.java
+++ 3.evaluación/Tema10/Repaso/Ejercicio17/src/ejercicio17/Ejercicio17.java
@@ -6,9 +6,12 @@
 package ejercicio17;
 
 import clases.*;
+import excepcion.datoSinSentido;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import javax.swing.JOptionPane;
 
 /**
@@ -26,6 +29,12 @@ public class Ejercicio17 {
     private static Articulo articulo;
     private static Vendedor vendedor;
     
+    private static String tipo;
+    
+    private static Matcher mat;
+    private static Pattern pat = Pattern.compile("^(C.)?[ ]?[A-z][a-z]*/[1]?[1-9]/[1]?[1-9][A-Z]$");
+    private static Pattern pat2 = Pattern.compile("^[A-z][a-z]*$");
+    
     public static void main(String[] args) {
         crearComprador();
         crearEnvio();
@@ -35,10 +44,25 @@ public class Ejercicio17 {
     }
 
     private static void crearComprador() {
-        String nombre=JOptionPane.showInputDialog("Introduzca nombre del comprador");
-        String apellido=JOptionPane.showInputDialog("Introduzca apellido del comprador");
-        String direccion=JOptionPane.showInputDialog("Introduzca dirección del comprador");
-        comprador = new Comprador(nombre, apellido, direccion);
+        try{
+            String nombre=JOptionPane.showInputDialog("Introduzca nombre del comprador");
+                tipo = "nombre";
+            comprobador(nombre);
+        
+            String apellido=JOptionPane.showInputDialog("Introduzca apellido del comprador");
+                tipo = "apellido";            
+            comprobador(apellido);
+        
+            String direccion=JOptionPane.showInputDialog("Introduzca dirección del comprador");
+                tipo = "direccion";
+            comprobador(direccion);
+        
+            comprador = new Comprador(nombre, apellido, direccion); 
+        }
+        
+        catch(Exception e){
+           JOptionPane.showMessageDialog(null, "Error, no se ha podido crear opjeto");
+        }
     }
 
     private static void crearEnvio() {
@@ -46,12 +70,14 @@ public class Ejercicio17 {
             SimpleDateFormat formato = new SimpleDateFormat("dd/MM/yyyy");
             Date fecha= formato.parse(JOptionPane.showInputDialog("Introduzca fecha de entrega"));
             String direccion = JOptionPane.showInputDialog("Introduzca dirección del envio");
-            
+                tipo = "direccion";
+            comprobador(direccion);
+        
             envio = new Envio(fecha, direccion, comprador);
             comprador.setEnvios(envio);
         }
         catch(Exception e){
-            
+           JOptionPane.showMessageDialog(null, "Error, no se ha podido crear opjeto");
         }
         
     }
@@ -59,6 +85,9 @@ public class Ejercicio17 {
     private static void crearArticulo() {
         try{
             String nombre=JOptionPane.showInputDialog("Introduzca nombre del articulo");
+                tipo = "nombre";
+            comprobador(nombre);
+        
             double precio = Double.parseDouble(JOptionPane.showInputDialog("Introduzca precio del articulo"));
             articulo = new Articulo (nombre, precio, envio);
             
@@ -66,14 +95,20 @@ public class Ejercicio17 {
         }
         
         catch(Exception e){
-            
+           JOptionPane.showMessageDialog(null, "Error, no se ha podido crear opjeto");
         }
     }
 
     private static void crearVendedor() {
         try{
-            String nombre=JOptionPane.showInputDialog("Introduzca nombre del vendedor");
-            String apellido=JOptionPane.showInputDialog("Introduzca apellido del vendedor");
+            String nombre = JOptionPane.showInputDialog("Introduzca nombre del vendedor");
+                tipo = "nombre";
+            comprobador(nombre);
+        
+            String apellido = JOptionPane.showInputDialog("Introduzca apellido del vendedor");
+                tipo = "apellido";
+            comprobador(apellido);
+        
             vendedor = new Vendedor(nombre, apellido);
             
             envio.setVendedor(vendedor);
@@ -84,7 +119,7 @@ public class Ejercicio17 {
         }
         
         catch (Exception e){
-            
+           JOptionPane.showMessageDialog(null, "Error, no se ha podido crear opjeto");
         }
     }
 
@@ -94,13 +129,29 @@ public class Ejercicio17 {
             Date fecha= formato.parse(JOptionPane.showInputDialog("Introduzca fecha de pago"));
             double importe = Double.parseDouble(JOptionPane.showInputDialog("Introduzca importe"));
             String direccion = JOptionPane.showInputDialog("Introduzca dirección de factura");
+                tipo = "direccion";
+            comprobador(direccion);
 
             pago = new Pago (fecha, importe, direccion, envio);
             envio.setPago(pago);
         }
         
         catch(Exception e){
+           JOptionPane.showMessageDialog(null, "Error, no se ha podido crear opjeto");
+        }
+    }
+
+    private static void comprobador(String dato) throws Exception {
+        switch(tipo){
+            case "direccion": mat= pat.matcher(dato);
+            if (!mat.matches()){
+                throw new datoSinSentido();
+            }
             
+            default: mat= pat2.matcher(dato);
+            if (!mat.matches()){
+                throw new datoSinSentido();
+            }
         }
     }
     
---
+++ 3.evaluación/Tema10/Repaso/Ejercicio18/src/clases/Docente.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Asus
+ */
+public class Docente {
+    private int IdDoc;
+    private String nombre;
+    private String profesion;
+    private String cedula;
+    private String email;
+}
